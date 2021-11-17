ab549d0bbf507c20d318130e1801ac91565e9253
Nabhanyusingh <83275019+Nabhanyusingh@users.noreply.github.com>
2021-11-17 15:22:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.attach('#camera');
+camera =  document.getElementById("camera");
+Webcam.set ({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
